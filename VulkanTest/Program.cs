69157e5c9a3e317613ac74f99fb44ee63c315421
Sebastian Loncar <sebastian.loncar@gmail.com>
2021-10-15 22:25:44 +0200

    na

--- VulkanTest/Program.cs
+++ VulkanTest/Program.cs
@@ -1,18 +1,12 @@
-﻿using System.Numerics;
-using Vortice;
-using Vortice.ShaderCompiler;
+﻿using Vortice.ShaderCompiler;
 using Vortice.Vulkan;
 using static Vortice.Vulkan.Vulkan;
 
 namespace DrawTriangle
 {
-    public static unsafe class Program
+    public static unsafe partial class Program
     {
-#if DEBUG
-        private static bool EnableValidationLayers = true;
-#else
-		private static bool EnableValidationLayers = false;
-#endif
+
         public static void Main()
         {
             string vertexShader = @"
@@ -44,58 +38,6 @@ void main()
 
             using var testApp = new TestApp();
             testApp.Run();
-        }
-
-        class TestApp : Application
-        {
-            private GraphicsDevice? _graphicsDevice;
-            private float _green = 0.0f;
-
-            public override string Name => "01-ClearScreen";
-
-            protected override void Initialize()
-            {
-                // Need to initialize
-                vkInitialize().CheckResult();
-
-                _graphicsDevice = new GraphicsDevice(Name, EnableValidationLayers, MainWindow);
-            }
-
-            public override void Dispose()
-            {
-                _graphicsDevice!.Dispose();
-
-                base.Dispose();
-            }
-
-            protected override void OnTick()
-            {
-                _graphicsDevice!.RenderFrame(OnDraw);
-            }
-
-            private void OnDraw(VkCommandBuffer commandBuffer, VkFramebuffer framebuffer, VkExtent2D size)
-            {
-                float g = _green + 0.001f;
-                if (g > 1.0f)
-                    g = 0.0f;
-                _green = g;
-
-                VkClearValue clearValue = new VkClearValue(1.0f, _green, 0.0f, 1.0f);
-
-                // Begin the render pass.
-                VkRenderPassBeginInfo renderPassBeginInfo = new VkRenderPassBeginInfo
-                {
-                    sType = VkStructureType.RenderPassBeginInfo,
-                    renderPass = _graphicsDevice!.Swapchain.RenderPass,
-                    framebuffer = framebuffer,
-                    renderArea = new VkRect2D(size),
-                    clearValueCount = 1,
-                    pClearValues = &clearValue
-                };
-                vkCmdBeginRenderPass(commandBuffer, &renderPassBeginInfo, VkSubpassContents.Inline);
-                vkCmdSetBlendConstants(commandBuffer, new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
-                vkCmdEndRenderPass(commandBuffer);
-            }
         }
     }
 }
---
+++ VulkanTest/TestApp.cs
@@ -0,0 +1,66 @@
+﻿using System.Numerics;
+using Vortice;
+using Vortice.Vulkan;
+using static Vortice.Vulkan.Vulkan;
+
+namespace DrawTriangle
+{
+    public unsafe class TestApp : Application
+    {
+
+#if DEBUG
+        private static bool EnableValidationLayers = true;
+#else
+		private static bool EnableValidationLayers = false;
+#endif
+
+        private GraphicsDevice? _graphicsDevice;
+        private float _green = 0.0f;
+
+        public override string Name => "01-ClearScreen";
+
+        protected override void Initialize()
+        {
+            // Need to initialize
+            vkInitialize().CheckResult();
+
+            _graphicsDevice = new GraphicsDevice(Name, EnableValidationLayers, MainWindow);
+        }
+
+        public override void Dispose()
+        {
+            _graphicsDevice!.Dispose();
+
+            base.Dispose();
+        }
+
+        protected override void OnTick()
+        {
+            _graphicsDevice!.RenderFrame(OnDraw);
+        }
+
+        private void OnDraw(VkCommandBuffer commandBuffer, VkFramebuffer framebuffer, VkExtent2D size)
+        {
+            float g = _green + 0.01f;
+            if (g > 1.0f)
+                g = 0.0f;
+            _green = g;
+
+            VkClearValue clearValue = new VkClearValue(1.0f, _green, 0.0f, 1.0f);
+
+            // Begin the render pass.
+            VkRenderPassBeginInfo renderPassBeginInfo = new VkRenderPassBeginInfo
+            {
+                sType = VkStructureType.RenderPassBeginInfo,
+                renderPass = _graphicsDevice!.Swapchain.RenderPass,
+                framebuffer = framebuffer,
+                renderArea = new VkRect2D(size),
+                clearValueCount = 1,
+                pClearValues = &clearValue
+            };
+            vkCmdBeginRenderPass(commandBuffer, &renderPassBeginInfo, VkSubpassContents.Inline);
+            vkCmdSetBlendConstants(commandBuffer, new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+            vkCmdEndRenderPass(commandBuffer);
+        }
+    }
+}
