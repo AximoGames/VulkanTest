ab2d459317a3ea3c6907b39ef226992cdade6bcf
Sebastian Loncar <sebastian.loncar@gmail.com>
2021-10-17 11:36:31 +0200

    moved MyGameWindow to seperate file

--- VulkanTest/Application.cs
+++ VulkanTest/Application.cs
@@ -3,26 +3,9 @@ using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
-using OpenTK.Windowing.Desktop;
-using OpenTK.Windowing.Common;
 
 namespace Vortice
 {
-    public class MyGameWindow : GameWindow
-    {
-        public MyGameWindow(string name) :
-         base(new GameWindowSettings { IsMultiThreaded = true }, new NativeWindowSettings { Title = name, API = ContextAPI.NoAPI })
-        {
-        }
-
-        public override void Run()
-        {
-            while (true)
-            {
-                OnRenderFrame(new FrameEventArgs());
-            }
-        }
-    }
 
     public abstract class Application
     {
