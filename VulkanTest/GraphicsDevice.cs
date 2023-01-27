3af60a430bf1b3833998298f8cb6fecd05e26b7c
Sebastian Loncar <sebastian.loncar@gmail.com>
2023-01-27 22:30:22 +0100

    update

--- VulkanTest/GraphicsDevice.cs
+++ VulkanTest/GraphicsDevice.cs
@@ -599,11 +599,11 @@ void main() {
             VkPhysicalDeviceMemoryProperties memProperties;
             vkGetPhysicalDeviceMemoryProperties(PhysicalDevice, out memProperties);
 
-            for (uint i = 0; i < memProperties.memoryTypeCount; i++)
+            for (int i = 0; i < memProperties.memoryTypeCount; i++)
             {
-                if ((typeFilter & (1 << (int)i)) != 0 && (memProperties.GetMemoryType(i).propertyFlags & properties) == properties)
+                if ((typeFilter & (1 << i)) != 0 && (memProperties.memoryTypes[i].propertyFlags & properties) == properties)
                 {
-                    return i;
+                    return (uint)i;
                 }
             }
 
