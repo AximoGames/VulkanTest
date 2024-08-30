5f6f455a526b866a9ebd4a03170aaed093862cdd
Sebastian Loncar <sebastian.loncar@gmail.com>
2024-08-30 15:56:20 +0200

    Refactor Vulkan instance handling into its own class
    Extract Vulkan instance creation and management logic into `VulkanInstance` class. This refactoring improves code modularity and reusability, simplifying the `GraphicsDevice` class by delegating Vulkan instance responsibilities to the new class.

--- VulkanTest/GraphicsDevice.cs
+++ VulkanTest/GraphicsDevice.cs
@@ -16,11 +16,9 @@ namespace Vortice;
 public unsafe sealed class GraphicsDevice : IDisposable
 {
     private static readonly string s_EngineName = "Vortice";
-    private static readonly string[] s_RequestedValidationLayers = new[] { "VK_LAYER_KHRONOS_validation" };
 
-    public readonly VkInstance VkInstance;
+    public readonly VulkanInstance VulkanInstance;
 
-    private readonly VkDebugUtilsMessengerEXT _debugMessenger = VkDebugUtilsMessengerEXT.Null;
     internal readonly VkSurfaceKHR _surface;
     public readonly VkPhysicalDevice PhysicalDevice;
     public readonly VkDevice VkDevice;
@@ -40,7 +38,7 @@ public unsafe sealed class GraphicsDevice : IDisposable
 
     public GraphicsDevice(string applicationName, bool enableValidation, GameWindow window)
     {
-        CreateInstance(applicationName, enableValidation, out VkInstance, out _debugMessenger);
+        VulkanInstance = new VulkanInstance(applicationName, enableValidation);
 
         _surface = CreateSurface(window);
 
@@ -71,111 +69,10 @@ public unsafe sealed class GraphicsDevice : IDisposable
         }
     }
 
-    private void CreateInstance(string applicationName, bool enableValidation, out VkInstance instance, out VkDebugUtilsMessengerEXT _debugMessenger)
-    {
-        var appInfo = new VkApplicationInfo
-        {
-            pApplicationName = applicationName.ToVkUtf8ReadOnlyString(),
-            applicationVersion = new VkVersion(1, 0, 0),
-            pEngineName = s_EngineName.ToVkUtf8ReadOnlyString(),
-            engineVersion = new VkVersion(1, 0, 0),
-            apiVersion = vkEnumerateInstanceVersion()
-        };
-
-        List<string> instanceExtensions = new List<string>();
-
-        instanceExtensions.AddRange(GLFW.GetRequiredInstanceExtensions());
-
-        // if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        // {
-        //     instanceExtensions.Add(KHRWin32SurfaceExtensionName);
-        // }
-
-        // if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        // {
-        //     instanceExtensions.Add(KHRXlibSurfaceExtensionName);
-        // }
-
-        List<string> instanceLayers = new List<string>();
-        if (enableValidation)
-        {
-            FindValidationLayers(instanceLayers);
-        }
-
-        if (instanceLayers.Count > 0)
-        {
-            instanceExtensions.Add(VK_EXT_DEBUG_UTILS_EXTENSION_NAME.GetStringFromUtf8Buffer());
-        }
-
-        //instanceExtensions.Add("VK_KHR_buffer_device_address");
-
-        using var vkInstanceExtensions = new VkStringArray(instanceExtensions);
-
-        var instanceCreateInfo = new VkInstanceCreateInfo
-        {
-            pApplicationInfo = &appInfo,
-            enabledExtensionCount = vkInstanceExtensions.Length,
-            ppEnabledExtensionNames = vkInstanceExtensions
-        };
-
-        using var vkLayerNames = new VkStringArray(instanceLayers);
-        if (instanceLayers.Count > 0)
-        {
-            instanceCreateInfo.enabledLayerCount = vkLayerNames.Length;
-            instanceCreateInfo.ppEnabledLayerNames = vkLayerNames;
-        }
-
-        var debugUtilsCreateInfo = new VkDebugUtilsMessengerCreateInfoEXT
-        {
-        };
-
-        if (instanceLayers.Count > 0)
-        {
-            debugUtilsCreateInfo.messageSeverity = VkDebugUtilsMessageSeverityFlagsEXT.Error | VkDebugUtilsMessageSeverityFlagsEXT.Warning;
-            debugUtilsCreateInfo.messageType = VkDebugUtilsMessageTypeFlagsEXT.Validation | VkDebugUtilsMessageTypeFlagsEXT.Performance | VkDebugUtilsMessageTypeFlagsEXT.General;
-            debugUtilsCreateInfo.pfnUserCallback = &DebugMessengerCallback;
-
-            instanceCreateInfo.pNext = &debugUtilsCreateInfo;
-        }
-
-        VkResult result = vkCreateInstance(&instanceCreateInfo, null, out instance);
-        if (result != VkResult.Success)
-        {
-            throw new InvalidOperationException($"Failed to create vulkan instance: {result}");
-        }
-
-        vkLoadInstance(VkInstance);
-
-        if (instanceLayers.Count > 0)
-        {
-            vkCreateDebugUtilsMessengerEXT(VkInstance, &debugUtilsCreateInfo, null, out _debugMessenger).CheckResult();
-        }
-        else
-        {
-            _debugMessenger = VkDebugUtilsMessengerEXT.Null;
-        }
-
-        Log.Info($"Created VkInstance with version: {appInfo.apiVersion.Major}.{appInfo.apiVersion.Minor}.{appInfo.apiVersion.Patch}");
-        if (instanceLayers.Count > 0)
-        {
-            foreach (var layer in instanceLayers)
-            {
-                Log.Info($"Instance layer '{layer}'");
-            }
-        }
-
-        foreach (string extension in instanceExtensions)
-        {
-            Log.Info($"Instance extension '{extension}'");
-        }
-
-        GetAvailabeExtensions();
-    }
-
-    public void CreateDevice(out VkPhysicalDevice physicalDevice, out VkDevice device, out VkQueue graphicsQueue, out VkQueue presentQueue)
+    private void CreateDevice(out VkPhysicalDevice physicalDevice, out VkDevice device, out VkQueue graphicsQueue, out VkQueue presentQueue)
     {
         // Find physical device, setup queue's and create device.
-        var physicalDevices = vkEnumeratePhysicalDevices(VkInstance);
+        var physicalDevices = vkEnumeratePhysicalDevices(VulkanInstance.Instance);
         foreach (var pDevice in physicalDevices)
         {
             //vkGetPhysicalDeviceProperties(pDevice, out var properties);
@@ -298,26 +195,6 @@ public unsafe sealed class GraphicsDevice : IDisposable
         vkGetDeviceQueue(device, queueFamilies.presentFamily, 0, out presentQueue);
     }
 
-    private void GetAvailabeExtensions()
-    {
-        uint count;
-        var result = vkEnumerateInstanceExtensionProperties(&count, null);
-        if (result != VkResult.Success)
-            throw new Exception($"Failed to enumerate instance extensions, {result}");
-
-        var properties = new VkExtensionProperties[count];
-        result = vkEnumerateInstanceExtensionProperties(properties);
-        if (result != VkResult.Success)
-            throw new Exception($"Failed to enumerate instance extensions, {result}");
-
-        Log.Verbose($"Found {count} instance extensions:");
-        foreach (var prop in properties)
-        {
-            var name = prop.GetExtensionName();
-            Log.Verbose(name);
-        }
-    }
-    
     public Vertex[] Vertices = new Vertex[]
     {
         new Vertex { pos = new Vector2(-0.5f, -0.5f), color = new Vector3(1.0f, 0.0f, 0.0f) },
@@ -568,18 +445,10 @@ public unsafe sealed class GraphicsDevice : IDisposable
 
         if (_surface != VkSurfaceKHR.Null)
         {
-            vkDestroySurfaceKHR(VkInstance, _surface, null);
+            vkDestroySurfaceKHR(VulkanInstance.Instance, _surface, null);
         }
 
-        if (_debugMessenger != VkDebugUtilsMessengerEXT.Null)
-        {
-            vkDestroyDebugUtilsMessengerEXT(VkInstance, _debugMessenger, null);
-        }
-
-        if (VkInstance != VkInstance.Null)
-        {
-            vkDestroyInstance(VkInstance, null);
-        }
+        VulkanInstance.Dispose();
     }
 
     public void RenderFrame(Action<VkCommandBuffer, VkExtent2D> draw, [CallerMemberName] string? frameName = null)
@@ -704,76 +573,8 @@ public unsafe sealed class GraphicsDevice : IDisposable
 
     private VkSurfaceKHR CreateSurface(GameWindow window)
     {
-        GLFW.CreateWindowSurface(new VkHandle(VkInstance.Handle), window.WindowPtr, null, out var handle);
+        GLFW.CreateWindowSurface(new VkHandle(VulkanInstance.Instance.Handle), window.WindowPtr, null, out var handle);
         return new VkSurfaceKHR((ulong)handle.Handle);
-    }
-
-    [UnmanagedCallersOnly]
-    private static uint DebugMessengerCallback(VkDebugUtilsMessageSeverityFlagsEXT messageSeverity,
-        VkDebugUtilsMessageTypeFlagsEXT messageTypes,
-        VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-        void* userData)
-    {
-        string? message = VkStringInterop.ConvertToManaged(pCallbackData->pMessage);
-        string prefix = messageTypes == VkDebugUtilsMessageTypeFlagsEXT.Validation ? "[Vulkan]: Validation: " : "[Vulkan]: ";
-
-        switch (messageSeverity)
-        {
-            case VkDebugUtilsMessageSeverityFlagsEXT.Error:
-                Log.Error($"{prefix}{messageSeverity} - {message}");
-                if (messageTypes == VkDebugUtilsMessageTypeFlagsEXT.Validation)
-                {
-                    throw new Exception(message);
-                }
-
-                break;
-            case VkDebugUtilsMessageSeverityFlagsEXT.Warning:
-                Log.Warn($"{prefix}{messageSeverity} - {message}");
-                break;
-            case VkDebugUtilsMessageSeverityFlagsEXT.Info:
-                Log.Info($"{prefix}{messageSeverity} - {message}");
-                break;
-            case VkDebugUtilsMessageSeverityFlagsEXT.Verbose:
-                Log.Verbose($"{prefix}{messageSeverity} - {message}");
-                break;
-        }
-
-        Debug.WriteLine($"{prefix}{messageSeverity} - {message}");
-
-        return VK_FALSE;
-    }
-
-    private static void FindValidationLayers(List<string> appendTo)
-    {
-        ReadOnlySpan<VkLayerProperties> availableLayers = vkEnumerateInstanceLayerProperties();
-
-        for (int j = 0; j < availableLayers.Length; j++)
-        {
-            var name = availableLayers[j].GetLayerName();
-            Log.Info($"Found Layer: {name}");
-        }
-
-        for (int i = 0; i < s_RequestedValidationLayers.Length; i++)
-        {
-            bool hasLayer = false;
-            for (int j = 0; j < availableLayers.Length; j++)
-            {
-                if (s_RequestedValidationLayers[i] == availableLayers[j].GetLayerName())
-                {
-                    hasLayer = true;
-                    break;
-                }
-            }
-
-            if (hasLayer)
-            {
-                appendTo.Add(s_RequestedValidationLayers[i]);
-            }
-            else
-            {
-                // TODO: Warn
-            }
-        }
     }
 
     private (uint graphicsFamily, uint presentFamily) queueFamilies;
---
+++ VulkanTest/VulkanInstance.cs
@@ -0,0 +1,208 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using Vortice.Vulkan;
+using static Vortice.Vulkan.Vulkan;
+
+namespace Vortice;
+
+public unsafe class VulkanInstance : IDisposable
+{
+    private static readonly string[] s_RequestedValidationLayers = new[] { "VK_LAYER_KHRONOS_validation" };
+
+    public VkInstance Instance;
+    private VkDebugUtilsMessengerEXT _debugMessenger = VkDebugUtilsMessengerEXT.Null;
+
+    public VulkanInstance(string applicationName, bool enableValidation)
+    {
+        CreateInstance(applicationName, enableValidation);
+    }
+
+    private void CreateInstance(string applicationName, bool enableValidation)
+    {
+        var appInfo = new VkApplicationInfo
+        {
+            pApplicationName = applicationName.ToVkUtf8ReadOnlyString(),
+            applicationVersion = new VkVersion(1, 0, 0),
+            pEngineName = "Vortice".ToVkUtf8ReadOnlyString(),
+            engineVersion = new VkVersion(1, 0, 0),
+            apiVersion = vkEnumerateInstanceVersion()
+        };
+
+        List<string> instanceExtensions = new List<string>();
+        instanceExtensions.AddRange(GLFW.GetRequiredInstanceExtensions());
+
+        List<string> instanceLayers = new List<string>();
+        if (enableValidation)
+        {
+            FindValidationLayers(instanceLayers);
+        }
+
+        if (instanceLayers.Count > 0)
+        {
+            instanceExtensions.Add(VK_EXT_DEBUG_UTILS_EXTENSION_NAME.GetStringFromUtf8Buffer());
+        }
+
+        using var vkInstanceExtensions = new VkStringArray(instanceExtensions);
+
+        var instanceCreateInfo = new VkInstanceCreateInfo
+        {
+            pApplicationInfo = &appInfo,
+            enabledExtensionCount = vkInstanceExtensions.Length,
+            ppEnabledExtensionNames = vkInstanceExtensions
+        };
+
+        using var vkLayerNames = new VkStringArray(instanceLayers);
+        if (instanceLayers.Count > 0)
+        {
+            instanceCreateInfo.enabledLayerCount = vkLayerNames.Length;
+            instanceCreateInfo.ppEnabledLayerNames = vkLayerNames;
+        }
+
+        var debugUtilsCreateInfo = new VkDebugUtilsMessengerCreateInfoEXT
+        {
+            messageSeverity = VkDebugUtilsMessageSeverityFlagsEXT.Error | VkDebugUtilsMessageSeverityFlagsEXT.Warning,
+            messageType = VkDebugUtilsMessageTypeFlagsEXT.Validation | VkDebugUtilsMessageTypeFlagsEXT.Performance | VkDebugUtilsMessageTypeFlagsEXT.General,
+            pfnUserCallback = &DebugMessengerCallback
+        };
+
+        if (instanceLayers.Count > 0)
+        {
+            instanceCreateInfo.pNext = &debugUtilsCreateInfo;
+        }
+
+        VkResult result = vkCreateInstance(&instanceCreateInfo, null, out Instance);
+        if (result != VkResult.Success)
+        {
+            throw new InvalidOperationException($"Failed to create vulkan instance: {result}");
+        }
+
+        vkLoadInstance(Instance);
+
+        if (instanceLayers.Count > 0)
+        {
+            vkCreateDebugUtilsMessengerEXT(Instance, &debugUtilsCreateInfo, null, out _debugMessenger).CheckResult();
+        }
+
+        LogInstanceInfo(appInfo, instanceLayers, instanceExtensions);
+    }
+
+    private void LogInstanceInfo(VkApplicationInfo appInfo, List<string> instanceLayers, List<string> instanceExtensions)
+    {
+        Log.Info($"Created VkInstance with version: {appInfo.apiVersion.Major}.{appInfo.apiVersion.Minor}.{appInfo.apiVersion.Patch}");
+        if (instanceLayers.Count > 0)
+        {
+            foreach (var layer in instanceLayers)
+            {
+                Log.Info($"Instance layer '{layer}'");
+            }
+        }
+
+        foreach (string extension in instanceExtensions)
+        {
+            Log.Info($"Instance extension '{extension}'");
+        }
+
+        GetAvailableExtensions();
+    }
+
+    private void GetAvailableExtensions()
+    {
+        uint count;
+        var result = vkEnumerateInstanceExtensionProperties(&count, null);
+        if (result != VkResult.Success)
+            throw new Exception($"Failed to enumerate instance extensions, {result}");
+
+        var properties = new VkExtensionProperties[count];
+        result = vkEnumerateInstanceExtensionProperties(properties);
+        if (result != VkResult.Success)
+            throw new Exception($"Failed to enumerate instance extensions, {result}");
+
+        Log.Verbose($"Found {count} instance extensions:");
+        foreach (var prop in properties)
+        {
+            var name = prop.GetExtensionName();
+            Log.Verbose(name);
+        }
+    }
+
+    private static void FindValidationLayers(List<string> appendTo)
+    {
+        ReadOnlySpan<VkLayerProperties> availableLayers = vkEnumerateInstanceLayerProperties();
+
+        for (int j = 0; j < availableLayers.Length; j++)
+        {
+            var name = availableLayers[j].GetLayerName();
+            Log.Info($"Found Layer: {name}");
+        }
+
+        for (int i = 0; i < s_RequestedValidationLayers.Length; i++)
+        {
+            bool hasLayer = false;
+            for (int j = 0; j < availableLayers.Length; j++)
+            {
+                if (s_RequestedValidationLayers[i] == availableLayers[j].GetLayerName())
+                {
+                    hasLayer = true;
+                    break;
+                }
+            }
+
+            if (hasLayer)
+            {
+                appendTo.Add(s_RequestedValidationLayers[i]);
+            }
+            else
+            {
+                Log.Warn($"Validation layer '{s_RequestedValidationLayers[i]}' not found.");
+            }
+        }
+    }
+
+    [UnmanagedCallersOnly]
+    private static uint DebugMessengerCallback(VkDebugUtilsMessageSeverityFlagsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageTypes,
+        VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* userData)
+    {
+        string? message = VkStringInterop.ConvertToManaged(pCallbackData->pMessage);
+        string prefix = messageTypes == VkDebugUtilsMessageTypeFlagsEXT.Validation ? "[Vulkan]: Validation: " : "[Vulkan]: ";
+
+        switch (messageSeverity)
+        {
+            case VkDebugUtilsMessageSeverityFlagsEXT.Error:
+                Log.Error($"{prefix}{messageSeverity} - {message}");
+                if (messageTypes == VkDebugUtilsMessageTypeFlagsEXT.Validation)
+                {
+                    throw new Exception(message);
+                }
+
+                break;
+            case VkDebugUtilsMessageSeverityFlagsEXT.Warning:
+                Log.Warn($"{prefix}{messageSeverity} - {message}");
+                break;
+            case VkDebugUtilsMessageSeverityFlagsEXT.Info:
+                Log.Info($"{prefix}{messageSeverity} - {message}");
+                break;
+            case VkDebugUtilsMessageSeverityFlagsEXT.Verbose:
+                Log.Verbose($"{prefix}{messageSeverity} - {message}");
+                break;
+        }
+
+        return VK_FALSE;
+    }
+
+    public void Dispose()
+    {
+        if (_debugMessenger != VkDebugUtilsMessengerEXT.Null)
+        {
+            vkDestroyDebugUtilsMessengerEXT(Instance, _debugMessenger, null);
+        }
+
+        if (Instance != VkInstance.Null)
+        {
+            vkDestroyInstance(Instance, null);
+        }
+    }
+}
