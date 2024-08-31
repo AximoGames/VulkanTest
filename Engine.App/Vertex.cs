cd2b498692451b8edbdf8a32ffa755dfe5334a7d
Sebastian Loncar <sebastian.loncar@gmail.com>
2024-08-31 22:43:53 +0200

    Refactor namespaces for consistency
    Updated the namespaces in Vertex.cs and Buffer.cs to ensure they fall under the correct structure. This change helps in maintaining a consistent and logical organization in the project.

--- Engine.App/Vertex.cs
+++ Engine.App/Vertex.cs
@@ -1,7 +1,7 @@
 using System.Runtime.InteropServices;
 using OpenTK.Mathematics;
 
-namespace Engine.Vulkan;
+namespace Engine.App;
 
 public struct Vertex
 {
--- Engine/Buffer.cs
+++ Engine/Buffer.cs
@@ -1,3 +1,4 @@
+namespace Engine;
 
 public class Buffer
 {
