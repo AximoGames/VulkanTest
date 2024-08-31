696068bb79ecd4e63a7658d36127aa97df4051dd
Sebastian Loncar <sebastian.loncar@gmail.com>
2024-08-31 17:16:38 +0200

    Refactor Vulkan project references and update pipeline builder
    Commented out PrivateAssets attribute in Vulkan project reference to resolve potential build issues. Removed unused binding and attribute description configurations from PipelineBuilder. Changed Vortice.Vulkan package reference to a direct DLL reference for consistency across the project.

--- Engine/PipeLineBuilder.cs
+++ Engine/PipeLineBuilder.cs
@@ -6,19 +6,9 @@ namespace Engine.Vulkan;
 
 public abstract class PipelineBuilder
 {
-    protected VkVertexInputBindingDescription _bindingDescription;
-    protected VkVertexInputAttributeDescription[] _attributeDescriptions;
-
     public abstract void ConfigureShader(string shaderCode, ShaderKind shaderKind);
     public abstract Buffer CreateVertexBuffer<T>(T[] vertices) where T : unmanaged;
     public abstract Buffer CreateIndexBuffer(ushort[] indices);
-
-    public void ConfigureBindingDescription(VkVertexInputBindingDescription bindingDescription)
-        => _bindingDescription = bindingDescription;
-
-    public void ConfigureAttributeDescriptions(VkVertexInputAttributeDescription[] attributeDescriptions)
-        => _attributeDescriptions = attributeDescriptions;
-
     public abstract void ConfigureVertexLayout(VertexLayoutInfo vertexLayoutInfo);
 }
 
