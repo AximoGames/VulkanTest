aa6a68a4e4575a89b923eb37ba4493c44090d049
Sebastian Loncar <sebastian.loncar@gmail.com>
2024-08-31 11:22:01 +0200

    Rename and remove unused imports and comments
    Refactored program by removing unnecessary using statements and obsolete GLSL shader code comments. Renamed TestApp.cs for better project structure and clarity.

--- VulkanTest/Program.cs
+++ VulkanTest/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using Vortice.ShaderCompiler;
-using Vortice.Vulkan;
-using static Vortice.Vulkan.Vulkan;
 
 namespace VulkanTest;
 
@@ -11,36 +8,6 @@ public static unsafe partial class Program
     {
         try
         {
-            // // language=glsl
-            // string vertexShader =
-            //     """
-            //     #version 450
-            //     layout (location = 0) in vec3 inPos;
-            //     layout (location = 1) in vec3 inColor;
-            //     layout (binding = 0) uniform UBO 
-            //     {
-            //     	mat4 projectionMatrix;
-            //     	mat4 modelMatrix;
-            //     	mat4 viewMatrix;
-            //     } ubo;
-            //     layout (location = 0) out vec3 outColor;
-            //     out gl_PerVertex 
-            //     {
-            //     	vec4 gl_Position;   
-            //     };
-            //     void main() 
-            //     {
-            //     	outColor = inColor;
-            //     	gl_Position = ubo.projectionMatrix * ubo.viewMatrix * ubo.modelMatrix * vec4(inPos.xyz, 1.0);
-            //     }
-            //     """;
-            //
-            // using Compiler compiler = new Compiler();
-            // using (var compilationResult = compiler.Compile(vertexShader, "main", ShaderKind.VertexShader))
-            // {
-            //     //vkCreateShaderModule(VkDevice, compilationResult.GetBytecode(), null, out VkShaderModule module).CheckResult();
-            // }
-
             var testApp = new TestApp();
             testApp.Run();
         }
