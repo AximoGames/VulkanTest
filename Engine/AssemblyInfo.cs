e6e1bbe9ac2299e2f078e3d26408abe16cadbf2a
Sebastian Loncar <sebastian.loncar@gmail.com>
2024-09-05 15:19:41 +0200

    Rename Vulkan components to Backend for better clarity
    Renamed the Vulkan-related components and directories to Backend.Vulkan and adjusted references accordingly. This improves the project structure and promotes a clearer separation of backend functionalities.

--- Engine/AssemblyInfo.cs
+++ Engine/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("Engine.Vulkan")]
+[assembly:InternalsVisibleTo("Engine.Backend.Vulkan")]
 [assembly:InternalsVisibleTo("Engine.Windowing.Sdl")]
