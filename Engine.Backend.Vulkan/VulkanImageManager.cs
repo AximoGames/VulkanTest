ccc89d12ce78a02910e78aebbea746008409b197
Sebastian Loncar <sebastian.loncar@gmail.com>
2024-09-08 18:14:32 +0200

    Add null check for render target in VulkanPassBuilder
    Ensure the render target is set before building a Vulkan pass. This prevents potential runtime exceptions caused by a missing render target.

--- Engine.Backend.Vulkan/VulkanImageManager.cs
+++ Engine.Backend.Vulkan/VulkanImageManager.cs
@@ -1,5 +1,6 @@
 using Vortice.Vulkan;
 using static Vortice.Vulkan.Vulkan;
+using OpenTK.Mathematics;
 
 namespace Engine.Vulkan;
 
@@ -16,7 +17,55 @@ internal unsafe class VulkanImageManager : BackendImageManager
 
     public override BackendImage CreateRenderTargetImage(uint width, uint height)
     {
-        throw new NotImplementedException();
+        VkImageCreateInfo imageInfo = new VkImageCreateInfo
+        {
+            imageType = VkImageType.Image2D,
+            extent = new VkExtent3D { width = width, height = height, depth = 1 },
+            mipLevels = 1,
+            arrayLayers = 1,
+            format = VkFormat.R8G8B8A8Unorm,
+            tiling = VkImageTiling.Optimal,
+            initialLayout = VkImageLayout.Undefined,
+            usage = VkImageUsageFlags.ColorAttachment | VkImageUsageFlags.TransferSrc,
+            sharingMode = VkSharingMode.Exclusive,
+            samples = VkSampleCountFlags.Count1
+        };
 
+        VkImage image;
+        vkCreateImage(_device.LogicalDevice, &imageInfo, null, out image).CheckResult();
+
+        VkMemoryRequirements memRequirements;
+        vkGetImageMemoryRequirements(_device.LogicalDevice, image, out memRequirements);
+
+        VkMemoryAllocateInfo allocInfo = new VkMemoryAllocateInfo
+        {
+            allocationSize = memRequirements.size,
+            memoryTypeIndex = _bufferManager.FindMemoryType(memRequirements.memoryTypeBits, VkMemoryPropertyFlags.DeviceLocal)
+        };
+
+        VkDeviceMemory imageMemory;
+        vkAllocateMemory(_device.LogicalDevice, &allocInfo, null, out imageMemory).CheckResult();
+
+        vkBindImageMemory(_device.LogicalDevice, image, imageMemory, 0).CheckResult();
+
+        VkImageViewCreateInfo viewInfo = new VkImageViewCreateInfo
+        {
+            image = image,
+            viewType = VkImageViewType.Image2D,
+            format = VkFormat.R8G8B8A8Unorm,
+            subresourceRange = new VkImageSubresourceRange
+            {
+                aspectMask = VkImageAspectFlags.Color,
+                baseMipLevel = 0,
+                levelCount = 1,
+                baseArrayLayer = 0,
+                layerCount = 1
+            }
+        };
+
+        VkImageView imageView;
+        vkCreateImageView(_device.LogicalDevice, &viewInfo, null, out imageView).CheckResult();
+
+        return new VulkanImage(_device, new Vector2i((int)width, (int)height), image, imageView, imageMemory, VkFormat.R8G8B8A8Unorm, true);
     }
 }
