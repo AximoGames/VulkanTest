81a5d312972c3fc305580eab0272f7d0cbd6c323
Sebastian Loncar <sebastian.loncar@gmail.com>
2024-09-10 22:34:58 +0200

    Refactor and optimize ClearScreenApp class.
    Rearranged class members, streamlined initialization, and optimized the green value calculation. Improved readability by combining statements and removing unused debug messages.

--- Engine.App/ClearScreenApp.cs
+++ Engine.App/ClearScreenApp.cs
@@ -10,30 +10,24 @@ namespace Engine.App;
 
 public class ClearScreenApp : Application
 {
-    private GraphicsDevice _graphicsDevice;
-    private float _greenValue = 0.0f;
-    private bool EnableValidationLayers = true;
-
-    private string[] suppressDebugMessages =
-    [
-        //"VUID-VkPresentInfoKHR-pImageIndices-01430",
-    ];
-
-    private Pass _drawPass;
-
     public override string Name => "02-ClearScreen";
+
+    private GraphicsDevice _graphicsDevice;
+    private const bool _enableValidationLayers = true;
+    
+    private Pass _drawPass;
+    private float _greenValue = 0.0f;
 
     protected override void Initialize()
     {
         var windowManager = SdlWindowManager.GetInstance();
         RegisterWindowManager(windowManager);
         var window = windowManager.CreateWindow(Name);
-        RenderFrame += (e) => { OnRenderFrame(); };
+        RenderFrame += OnRenderFrame;
 
-        _graphicsDevice = new GraphicsDevice(VulkanGraphicsFactory.CreateVulkanGraphicsDevice(window, Name, EnableValidationLayers, suppressDebugMessages));
+        _graphicsDevice = new GraphicsDevice(VulkanGraphicsFactory.CreateVulkanGraphicsDevice(window, Name, _enableValidationLayers));
         CreatePasses();
     }
-
 
     private void CreatePasses()
     {
@@ -51,17 +45,15 @@ public class ClearScreenApp : Application
         });
     }
 
-    protected override void OnRenderFrame()
+    private void OnRenderFrame(FrameEventArgs args)
     {
-        float g = _greenValue + 0.0003f;
-        if (g > 1.0f)
-            g = 0.0f;
-        _greenValue = g;
+        _greenValue = (_greenValue + 0.0003f) % 1.0f;
 
         _graphicsDevice.RenderFrame(frameContext =>
         {
             frameContext.UsePass(_drawPass, passContext =>
             {
+                // Clear
                 passContext.Clear(new Color3<Rgb>(0.0f, _greenValue, 0.0f));
             });
         });
